a06de4379c66338a3f8c8204d4c5cb87473efd1e
Jesse Cardenas <jcardenas@liquidweb.com>
2020-05-29 19:36:20 -0500

    single line

--- command.py
+++ command.py
@@ -4,12 +4,7 @@ import argparse
 def get_args():
     """Get command line arguments"""
     parser = argparse.ArgumentParser(description='Python cli boilerplate', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-n',
-        '--num',
-        help='A number',
-        metavar='int',
-        type=int,
-        default=10)
+    parser.add_argument('-n', '--num', help='A number', metavar='int', type=int, default=10)
     args = parser.parse_args()
     return args
 
